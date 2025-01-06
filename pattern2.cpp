3e154b5404131b7e0efe56e44deb062d182377af
abhishek <abhishektk18@gmail.com>
2025-01-06 21:40:32 +0530

    pattern 10 and strts

--- pattern2.cpp
+++ pattern2.cpp
@@ -84,10 +84,62 @@ void print7(int n)
         cout << endl;
     }
 }
+
+void print8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 1; j <= n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        // star
+
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+
+        // space
+        for (int j = 1; j <= n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print9(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        // star
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+
+        // space
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n;
     cout << "enter the number";
     cin >> n;
-    print7(n);
+    print8(n);
+    print9(n);
 }
