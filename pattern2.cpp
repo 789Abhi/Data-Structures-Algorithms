24e79a9090fd77cb7b2d4d3d6a129dcf4f6f87ce
abhishek <abhishektk18@gmail.com>
2025-01-13 21:05:01 +0530

    changes on prints

--- pattern2.cpp
+++ pattern2.cpp
@@ -110,6 +110,31 @@ void print8(int n)
         cout << endl;
     }
 }
+void print8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 1; j <= n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        // star
+
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+
+        // space
+        for (int j = 1; j <= n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
 
 void print9(int n)
 {
@@ -142,6 +167,7 @@ int main()
     cin >> n;
     print8(n);
     print8(n);
+    print9(n);
     print9(n);
     print9(n);
     print9(n);
